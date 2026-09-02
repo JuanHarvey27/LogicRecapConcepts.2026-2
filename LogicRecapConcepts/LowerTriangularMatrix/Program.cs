3b08a7dcc40825aeb255565c33db9cf7d8115b45
JuanHarvey27 <juanpat27@gmail.com>
2026-09-02 17:16:30 -0500

    In Exercise 31, I added the input of the matrix size and its creation during execution.

--- LogicRecapConcepts/LowerTriangularMatrix/Program.cs
+++ LogicRecapConcepts/LowerTriangularMatrix/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the order of the matrix:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[,] matrix = new int[n, n];
+
+        // The part responsible for creating the matrix on the go
+
+        for (int i=0;i<n;i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine($"{i+j,-4}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
