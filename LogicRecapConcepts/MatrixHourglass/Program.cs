b3235885286444be9d3ad8e9d51e59b54b5a1e93
JuanHarvey27 <juanpat27@gmail.com>
2026-09-02 18:35:38 -0500

    I started by doing Exercise 34. I used a while loop to verify that the entered data is correct, and then I created the Matrix.

--- LogicRecapConcepts/MatrixHourglass/Program.cs
+++ LogicRecapConcepts/MatrixHourglass/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the order of the matrix:");
+        int n;
+        while (!int.TryParse(Console.ReadLine(), out n) || n <= 0 || n % 2 == 0)
+        {
+            Console.WriteLine("Please enter a valid positive odd integer:");
+        }
+
+        // This is the part responsible for creating the matrix using the formula: Matrix[i, j] = 2*i + j
+        int[,] matrix = new int[n, n];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = (2 * i) + j;
+            }
+        }
+
+        // Show the complete original matrix
+    }
+}
