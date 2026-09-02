6545a99b6de9d657e5a0f6e84cfeba2d86e219f9
JuanHarvey27 <juanpat27@gmail.com>
2026-09-02 17:59:57 -0500

    The output of the original array and the lower triangular array was corrected. In addition, a while`loop was added to verify that the entered value is correct.

--- LogicRecapConcepts/LowerTriangularMatrix/Program.cs
+++ LogicRecapConcepts/LowerTriangularMatrix/Program.cs
@@ -5,16 +5,40 @@ class Program
     static void Main()
     {
         Console.WriteLine("Enter the order of the matrix:");
-        int n = Convert.ToInt32(Console.ReadLine());
+        int n;
+        while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid positive integer:");
+        }
+
+        // The part responsible for creating the matrix
         int[,] matrix = new int[n, n];
-
-        // The part responsible for creating the matrix on the go
-
-        for (int i=0;i<n;i++)
+        for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                Console.WriteLine($"{i+j,-4}");
+                matrix[i,j] = i+ j;    
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        // The part responsible for creating the lower triangular matrix
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
             }
             Console.WriteLine();
         }
